05d93ba5a8e04c6ff08e3556a9fdc989ec3a9e3f
[SW5] 김진규 <rlawlsrb16736@gmail.com>
2023-11-25 01:36:57 +0900

    Style: 로그인페이지 디자인 변경

--- src/pages/UserLogin/UserLogin.jsx
+++ src/pages/UserLogin/UserLogin.jsx
@@ -45,43 +45,36 @@ export default function UserLogin() {
     };
 
     return (
-        <S.Container>
+        <S.Bg>
             <S.LoginBox>
-                <S.Logo
-                    src="https://tasty-together.s3.ap-northeast-2.amazonaws.com/main/default-profile-image.png"
-                    alt="logo"
-                />
-                <h1>Login</h1>
+                <S.Title>Login</S.Title>
                 <S.Form onSubmit={login}>
-                    <ul>
-                        <li>
-                            <input
-                                name="email"
-                                type="text"
-                                onChange={(e) => {
-                                    setEmail(e.target.value);
-                                }}
-                                placeholder={location.state?.email || '이메일(ID)'}
-                                value={email}
-                            />
-                        </li>
-                        <li>
-                            <input
-                                name="password"
-                                type="password"
-                                placeholder="비밀번호"
-                                onChange={(e) => {
-                                    setPassword(e.target.value);
-                                }}
-                                value={password}
-                            />
-                        </li>
-                    </ul>
-                    <button type="submit">로그인</button>
+                    <S.Input
+                        name="email"
+                        type="text"
+                        onChange={(e) => {
+                            setEmail(e.target.value);
+                        }}
+                        placeholder="Email"
+                        value={email}
+                    />
+                    <S.Input
+                        name="password"
+                        type="password"
+                        placeholder="Password"
+                        onChange={(e) => {
+                            setPassword(e.target.value);
+                        }}
+                        value={password}
+                    />
+                    <S.ErrorMsg isErr={errMsg}>{errMsg}</S.ErrorMsg>
+                    <S.SubmitBtn type="submit">Login</S.SubmitBtn>
                 </S.Form>
-                <S.LinkToSignUp to="/users/signup">회원가입 페이지로 이동</S.LinkToSignUp>
-                <S.ErrorMsg>{errMsg}</S.ErrorMsg>
+                <S.SignupText>
+                    아직 계정이 없으신가요?{' '}
+                    <S.Signup onClick={() => navigate('/users/signup')}>회원 가입하기</S.Signup>
+                </S.SignupText>
             </S.LoginBox>
-        </S.Container>
+        </S.Bg>
     );
 }
--- src/pages/UserLogin/style/UserLogin.style.jsx
+++ src/pages/UserLogin/style/UserLogin.style.jsx
@@ -1,81 +1,93 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
-export const Container = styled.div`
-    min-height: 100vh; // 페이지 높이를 100vh로 설정하여 스크롤을 내려야 footer가 보이게 설정
+export const Bg = styled.div`
+    min-height: 100vh;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
+    background: linear-gradient(to bottom, var(--color-accent), white);
 `;
 
 export const LoginBox = styled.div`
-    width: 400px;
-    height: 500px;
+    width: 500px;
+    height: 600px;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    border: 2px solid var(--color-accent);
+    border: none;
     position: relative;
     border-radius: 10px;
-    background-color: rgba(255, 255, 255, 0.9);
-    font-size: 20px;
+    background: rgba(255, 255, 255, 0.5);
 `;
 
-export const Logo = styled.img`
-    width: 100px;
-    position: absolute;
-    top: -50px;
+export const Title = styled.h1`
+    color: black;
+    font-size: 40px;
+    width: 100%;
+    height: 20%;
+    display: flex;
+    justify-content: center;
+    align-items: flex-end;
 `;
 
 export const Form = styled.form`
-    height: 50%;
+    width: 100%;
+    height: 60%;
     display: flex;
     flex-direction: column;
-    justify-content: space-around;
-    margin-top: 20px;
+`;
 
-    li {
-        input {
-            padding-left: 10px;
-            border: 1px solid var(--color-accent);
-            font-size: 17px;
-            width: 300px;
-            height: 50px;
-            border-radius: 10px;
-            display: block;
-            background-color: white;
-
-            &:hover {
-                outline: 1px solid var(--color-accent);
-                background-color: white;
-            }
-            &:focus {
-                outline: 1px solid var(--color-accent);
-                background-color: white;
-            }
-        }
+export const Input = styled.input`
+    width: 90%;
+    height: 15%;
+    background-color: white;
+    font-size: 18px;
+    font-weight: bold;
+    padding: 0 25px;
+    transition: all 250ms ease-out;
+    border-radius: 2rem;
+    border: 1px solid transparent;
+    outline: none;
+    cursor: pointer;
+    &:hover,
+    &:focus {
+        border: 1px solid var(--color-accent);
     }
-
-    li:last-child {
-        margin-top: 20px;
+    &:nth-of-type(1) {
+        margin: 10% auto 2.5%;
     }
-    button {
-        height: 50px;
-        background-color: rgba(255, 145, 77, 0.7);
-        border: none;
-        border-radius: 10px;
-        font-size: 17px;
-        color: white;
-        font-weight: bold;
-        display: block;
+    &:nth-of-type(2) {
+        margin: 2.5% auto 5%;
+    }
+`;
 
-        transition: 0.1s;
-        &:hover {
-            background-color: rgba(255, 145, 77);
-            transition: 0.3s;
-        }
+export const ErrorMsg = styled.p`
+    width: 100%;
+    height: 5%;
+    color: red;
+    font-size: 14px;
+    font-weight: 500;
+    text-align: center;
+    visibility: ${(props) => (props.isErr === '' ? 'hidden' : 'visible')};
+`;
+
+export const SubmitBtn = styled.button`
+    width: 90%;
+    height: 15%;
+    background-color: black;
+    border: none;
+    border-radius: 2rem;
+    font-size: 22px;
+    color: white;
+    font-weight: bold;
+    transition: 0.1s;
+    margin: 5% auto;
+    &:hover {
+        background-color: rgba(255, 145, 77);
+        transition: 0.3s;
     }
 `;
 
@@ -96,11 +108,15 @@ export const LinkToSignUp = styled(Link)`
     }
 `;
 
-export const ErrorMsg = styled.span`
-    color: red;
-    font-size: 13px;
-    font-weight: 500;
-    display: inline-block;
-    position: absolute;
-    bottom: 15px;
+export const SignupText = styled.p`
+    font-weight: 400;
+    color: black;
+    font-size: 18px;
+    height: 20%;
+`;
+
+export const Signup = styled.span`
+    text-decoration-line: underline;
+    font-weight: 600;
+    cursor: pointer;
 `;
